451c49e93356b91608ccdb6087263f9545c13062
Thomricco <tommasoricco@hotmail.it>
2022-01-17 15:33:54 +0100

    parte 1

---
+++ js/script.js
@@ -0,0 +1,39 @@
+new Vue({
+    el: "#app",
+    data: {
+        mansioneNonFatta: false,
+        currentIndex: 0,
+        todoList: [
+            {
+                text: 'Fare i compiti',
+                done: false,
+            },
+            {
+                text: 'Fare la spesa',
+                
+                done: false,
+            },
+            {
+                text: 'Fare il bucato',
+                done: true,
+            },
+            
+        ],
+        textTemoporaneo: '',
+        
+    },
+
+
+    methods: {
+        AddAList: function() {
+            this.todoList.push(this.textTemoporaneo)
+            this.textTemoporaneo = ''
+        },
+
+        removeAList: function(index) {
+            this.todoList.splice(index,1)
+        }
+        
+
+    }
+})
