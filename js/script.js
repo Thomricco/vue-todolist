4be3e05e9de7968243fc878b8fbea9d8f23fb6f4
Thomricco <tommasoricco@hotmail.it>
2022-01-18 10:13:27 +0100

    fine correzione

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 new Vue({
     el: "#app",
     data: {
-        isActive: true,
-        currentIndex: 0,
+        
         todoList: [
             {
                 text: 'Fare i compiti',
@@ -25,13 +24,17 @@ new Vue({
 
     methods: {
         AddAList: function() {
-            this.todoList.push({text: this.textTemoporaneo, done: ''})
+            this.todoList.push({text: this.textTemoporaneo, done: false})
             this.textTemoporaneo = ''
         },
 
         removeAList: function(index) {
             this.todoList.splice(index,1)
         },
+
+        doneUndone: function (index) {
+            this.todoList[index].done = ! this.todoList[index].done
+        }
         
     }
 })
